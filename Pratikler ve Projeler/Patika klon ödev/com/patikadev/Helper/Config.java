88bcdfcd3ffe988de682ac88459b39511e394795
Zubeyir-YILDIZ <115133461+Zubeyir-YILDIZ@users.noreply.github.com>
2023-03-09 06:26:19 +0300

    Update Config.java

--- Pratikler ve Projeler/Patika klon ödev/com/patikadev/Helper/Config.java
+++ Pratikler ve Projeler/Patika klon ödev/com/patikadev/Helper/Config.java
@@ -5,6 +5,6 @@ public class Config
     public static final String PROJECT_TITLE="Patika.Dev";
     public static final String DB_URL="jdbc:mysql://localhost/user";
     public static final String DB_USERNAME="root";
-    public static final String DB_PASSWORD="mysql1234";
+    public static final String DB_PASSWORD="ŞİFRE";
 
 }
