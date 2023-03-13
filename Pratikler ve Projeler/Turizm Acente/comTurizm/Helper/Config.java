0e94db0719011c08cb6e3ad859acfd18c45d2fb6
Zubeyir-YILDIZ <115133461+Zubeyir-YILDIZ@users.noreply.github.com>
2023-03-13 10:42:41 +0300

    Update Config.java

--- Pratikler ve Projeler/Turizm Acente/comTurizm/Helper/Config.java
+++ Pratikler ve Projeler/Turizm Acente/comTurizm/Helper/Config.java
@@ -5,6 +5,6 @@ public class Config
     public static final String PROJECT_TITLE="Turizm Acente";
     public static final String DB_URL="jdbc:mysql://localhost/acente";
     public static final String DB_USERNAME="root";
-    public static final String DB_PASSWORD="mysql1234";
+    public static final String DB_PASSWORD="ŞİFRE";
 
 }
